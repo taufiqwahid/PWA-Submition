8ebe6bcef2e1c7643461a10b0051beea90e9cfa9
Taufiq Wahid <taufiq.wahid58@gmail.com>
2020-10-05 19:43:05 +0800

    load page content nav

--- js/nav.js
+++ js/nav.js
@@ -21,4 +21,33 @@ document.addEventListener("DOMContentLoaded", function () {
     xhttp.open("GET", "nav.html", true);
     xhttp.send();
   }
+
+  // load page content
+  var page = window.location.hash.substr(1);
+  if (page == "") {
+    page = "home";
+  }
+  loadPage(page);
+
+  function loadPage(page) {
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function () {
+      if (this.readyState == 4) {
+        var content = document.querySelector("#body-content");
+        if (this.status == 200) {
+          content.innerHTML = xhttp.responseText;
+        } else if (this.status == 404) {
+          content.innerHTML =
+            "<p class='center-align'><b>404</b> Halaman tidak ditemukan</p>";
+        } else {
+          content.innerHTML =
+            "<p class='center-align'><b>Halaman tidak dapat di Akses!!</b></p>";
+        }
+      }
+    };
+    xhttp.open("GET", `pages/${page}.html`, true);
+    xhttp.send();
+  }
+
+  //
 });
